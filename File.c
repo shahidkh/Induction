54cf6d99f5de52ced82c5229b531a9503b8cba8b
shahidkh <shahidkh@thoughtworks.com>
2014-03-19 15:56:45 +0530

    Sample program
    This program prints entered number.

---
+++ File.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    while(T--)
+    {
+        int N;
+        scanf("%d",&N);
+        printf("You entered : %d\n",N);
+    }
+    return 0;
+}
